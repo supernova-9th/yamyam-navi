37176d74ac691d0cca295a65677ce55f14961221
nyximos <nyximos@gmail.com>
2024-08-29 01:32:22 +0900

    docs: swagger 어노테이션 추가 (#5)

--- src/main/java/com/yamyamnavi/api/CityController.java
+++ src/main/java/com/yamyamnavi/api/CityController.java
@@ -2,6 +2,8 @@ package com.yamyamnavi.api;
 
 import com.yamyamnavi.domain.city.CityService;
 import com.yamyamnavi.support.response.ResultResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +15,7 @@ import java.util.Map;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/v1/apis/cities")
+@Tag(name = "City API", description = "도시 정보를 조회하는 API")
 public class CityController {
 
     private final CityService cityService;
@@ -24,6 +27,7 @@ public class CityController {
      *             value 시/군/구 리스트
      */
     @GetMapping
+    @Operation(summary = "도시 목록 조회", description = "도시 및 시/군/구의 리스트를 조회합니다.")
     public ResultResponse<Map<String, List<String>>> getCites() {
         return new ResultResponse<>(cityService.getCites());
     }
