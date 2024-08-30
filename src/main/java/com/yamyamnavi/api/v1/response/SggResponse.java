5ae07845ee6811b4306a4f3062de189f2a7e587b
nyximos <nyximos@gmail.com>
2024-08-30 15:29:50 +0900

    docs: Swagger 어노테이션 추가 (#5)

--- src/main/java/com/yamyamnavi/api/v1/response/SggResponse.java
+++ src/main/java/com/yamyamnavi/api/v1/response/SggResponse.java
@@ -1,4 +1,11 @@
 package com.yamyamnavi.api.v1.response;
 
-public record SggResponse(String name, double longitude, double latitude) {
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "시군구 정보 응답")
+public record SggResponse(
+        @Schema(description = "시군구 이름") String name,
+        @Schema(description = "경도") double longitude,
+        @Schema(description = "위도") double latitude
+) {
 }
