70ead0188bdee3ddc77641dc3c4c993b997bcee1
nyximos <nyximos@gmail.com>
2024-08-30 15:28:33 +0900

    docs: Swagger 어노테이션 추가 (#14)

--- src/main/java/com/yamyamnavi/api/v1/response/ReviewResponse.java
+++ src/main/java/com/yamyamnavi/api/v1/response/ReviewResponse.java
@@ -1,12 +1,15 @@
 package com.yamyamnavi.api.v1.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import java.time.LocalDateTime;
 
+@Schema(description = "리뷰 정보 응답")
 public record ReviewResponse(
-        Long id,
-        String content,
-        Double score,
-        Long userId,
-        LocalDateTime createdAt
+        @Schema(description = "아이디") Long id,
+        @Schema(description = "내용") String content,
+        @Schema(description = "평점") Double score,
+        @Schema(description = "작성자 아이디") Long userId,
+        @Schema(description = "작성 일시") LocalDateTime createdAt
 ) {
 }
