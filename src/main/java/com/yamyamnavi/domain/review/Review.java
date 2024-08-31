e15fef25c8445e10a1e6caa5e3dad427427a82b1
nyximos <76250754+nyximos@users.noreply.github.com>
2024-08-31 14:07:00 +0900

    Revert "feat: 리뷰 생성 기능 구현"

--- src/main/java/com/yamyamnavi/domain/review/Review.java
+++ src/main/java/com/yamyamnavi/domain/review/Review.java
@@ -6,14 +6,12 @@ import java.time.LocalDateTime;
 
 @Getter
 @Setter
-@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class Review {
     private Long id;
-    private Long userId;
-    private Long restaurantId;
     private String content;
-    private Integer score;
+    private Double score;
+    private Long userId;
     private LocalDateTime createdAt;
 }
--- src/main/java/com/yamyamnavi/storage/review/ReviewCoreRepository.java
+++ src/main/java/com/yamyamnavi/storage/review/ReviewCoreRepository.java
@@ -2,7 +2,6 @@ package com.yamyamnavi.storage.review;
 
 import com.yamyamnavi.domain.review.Review;
 import com.yamyamnavi.domain.review.ReviewRepository;
-import com.yamyamnavi.storage.review.converter.ReviewConverter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -10,22 +9,10 @@ import java.util.List;
 
 @Component
 @RequiredArgsConstructor
-@Component
 public class ReviewCoreRepository implements ReviewRepository {
-
     private final ReviewJpaRepository reviewJpaRepository;
-    private final ReviewConverter reviewConverter;
     private final ReviewRepositoryImpl reviewRepositoryImpl;
 
-
-    @Override
-    public Review save(Review review) {
-
-        ReviewEntity reviewEntity = reviewConverter.convertToEntity(review);
-        ReviewEntity savedReview = reviewJpaRepository.save(reviewEntity);
-        return reviewConverter.convertToDomain(savedReview);
-    }
-  
     @Override
     public List<Review> findAllByRestaurantId(Long restaurantId) {
         return reviewRepositoryImpl.selectAllByRestaurantId(restaurantId);
