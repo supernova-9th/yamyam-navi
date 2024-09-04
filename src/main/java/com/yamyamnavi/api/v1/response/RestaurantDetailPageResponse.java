9afbc19700c37c4cd2e99af03642fa655814c3a2
nyximos <nyximos@gmail.com>
2024-09-04 12:21:19 +0900

    feat: request, response 추가 (#38)

---
+++ src/main/java/com/yamyamnavi/api/v1/response/RestaurantDetailPageResponse.java
@@ -0,0 +1,20 @@
+package com.yamyamnavi.api.v1.response;
+
+import com.yamyamnavi.support.response.PageResponse;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "맛집 정보 응답")
+public record RestaurantDetailPageResponse(
+        @Schema(description = "아이디") Long id,
+        @Schema(description = "매장명") String name,
+        @Schema(description = "지번 주소") String jibeonAddress,
+        @Schema(description = "도로명 주소") String roadAddress,
+        @Schema(description = "경도") double longitude,
+        @Schema(description = "위도") double latitude,
+        @Schema(description = "카테고리") String category,
+        @Schema(description = "영업 상태") Boolean isBusinessActive,
+        @Schema(description = "전화번호") String telephone,
+        @Schema(description = "평점") double score,
+        @Schema(description = "리뷰 목록") PageResponse<ReviewDetailResponse> pageReviews
+) {
+}
